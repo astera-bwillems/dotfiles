2393617e977aaaf7aff2e4d3dd3ce5c5148253d0
Brandt Willems <brandt.willems@asteralabs.com>
2022-12-29 12:08:27 -0800

    working on lab linux machine v0.8.0

--- lua/brandt/packer.lua
+++ lua/brandt/packer.lua
@@ -3,18 +3,18 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- Telescope
-  --use {
-  --  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-  ---- or                            , branch = '0.1.x',
-  --  requires = { {'nvim-lua/plenary.nvim'} },
-  --}
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} },
+  }
   --use({ "nvim-telescope/telescope-symbols.nvim" })
   --use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
   --use({ "nvim-telescope/telescope-file-browser.nvim" })
 
   use({
     'nvim-treesitter/nvim-treesitter',
-    commit = 'eadfcad580edc74b258dc13u0f1e3edea35cc6d4',
+--    commit = 'eadfcad580edc74b258dc13u0f1e3edea35cc6d4',
     run = ':TSUpdate'
   })
   --use("romgrk/nvim-treesitter-context")
