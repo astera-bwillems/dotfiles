d1a17cd8524bbb8f11a9e2175edbda9eb3eddfd1
Brandt Willems <brandt.willems@asteralabs.com>
2022-12-28 16:03:20 -0800

    nothing

--- snippets/all.lua
+++
@@ -1,8 +0,0 @@
--- SNIP_ENV defined in luasnip setup
-require("luasnip.loaders.from_lua").lazy_load()
-local env = SNIP_ENV
-
-return {
-    env.s("date", env.p(os.date, "%Y-%m-%d")),
-    env.s("time", env.p(os.date, "%H:%M")),
-}
