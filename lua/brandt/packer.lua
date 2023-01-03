70caafc2fc49cd5e4e2ac8fffe02b2019ed156d4
Brandt Willems <brandt.willems@asteralabs.com>
2023-01-03 13:28:31 -0800

    minimal working

--- lua/brandt/packer.lua
+++ lua/brandt/packer.lua
@@ -1,3 +1,11 @@
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -8,9 +16,9 @@ return require('packer').startup(function(use)
   -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} },
   }
-  --use({ "nvim-telescope/telescope-symbols.nvim" })
+  use({ "nvim-telescope/telescope-symbols.nvim" })
   --use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-  --use({ "nvim-telescope/telescope-file-browser.nvim" })
+  use({ "nvim-telescope/telescope-file-browser.nvim" })
 
   use({
     'nvim-treesitter/nvim-treesitter',
